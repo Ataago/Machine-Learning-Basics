88e14eb532d128b3044748f51dd1ac784e6ea8e5
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2019-08-24 08:38:29 +0530

    Decision Tree
    Clasifier - Decision Tree from sklearn

---
+++ classifier0.py
@@ -0,0 +1,26 @@
+# 
+#   clasifier0.py
+#   Supervised Learning
+#
+#   Created by Mohammed Ataa on 24/08/19.
+#   Copyright © 2019 Ataago. All rights reserved.
+#   
+#   Clasifier - Decision Tree
+#   Collect Training Data -> Train Clasifier -> make Predictions
+#   
+
+from sklearn import tree
+
+orange = 1
+apple = 0
+bumppy = 1
+smooth = 0
+
+# Training Data
+features = [[140, smooth], [130, smooth], [150, bumppy], [170, bumppy]]     # [100 grams, smooth surface]
+labels = [apple, apple, orange, orange]                                     
+
+clf = tree.DecisionTreeClassifier()     # Creating a Decision Tree Object
+clf = clf.fit(features, labels)     # Training classifier
+
+print(clf.predict([[149, smooth], [150, bumppy]]))  # Prediction
